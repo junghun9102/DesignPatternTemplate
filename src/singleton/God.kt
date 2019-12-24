18eff4954cf777aa5f217b9b3c31e5e113698a01
jh <junghun9102@gmail.com>
2019-12-24 23:41:10 +0900

    #1 Singleton : 자바에서 DCL 의 문제점
     - 심각한 오류! : singleton 기본인 생성자 제한이 없어서 추가 private constructor

--- src/singleton/God.kt
+++ src/singleton/God.kt
@@ -1,17 +1,26 @@
 package singleton
 
-class God(private val index: Int) {
+class God private constructor(private val index: Int) {
 
     companion object {
+
+        /**
+         *
+         * Volatile : Thread 를 처리중인 CPU cache memory 가 아닌 Main Memory 에 저장
+         *            한 개의 스레드에서면 값을 변경하고 대신 나머지 스레드는 항상 최신의 값을 확보할 수 있다.
+         *
+         */
+
+        @Volatile
         private var uniqueInstance: God? = null
 
-        @Synchronized
-        fun getInstance(): God {
-            uniqueInstance ?: run {
-                uniqueInstance = God(count++)
+        fun getInstance() = uniqueInstance ?: run {
+            synchronized(God) {
+                uniqueInstance ?: run {
+                    uniqueInstance = God(count++)
+                }
+                uniqueInstance!!
             }
-
-            return uniqueInstance!!
         }
 
         private var count = 0
--- src/singleton/WorldTest.kt
+++ src/singleton/WorldTest.kt
@@ -10,6 +10,12 @@ package singleton
  * -> 동기화를 통해 getInstance 에 동시 접근하지 못하도록 한다.
  *
  * Problem. 생성할 때만 조심하면 되는데 사용할 때도 동기화로 인한 성능 저하가 생긴다.
+ * -> DCL(Double Checking Locking)을 사용한다.
+ * -> null 이 아닌 경우에는 동기화없이 바로 인스턴스를 리턴해준다.
+ * -> null 인 경우에만 동기화를 통해 한 개의 인스턴스만 만들 것을 보장한다.
+ *
+ * Problem. 하지만 자바의 out of order writes 문제로 인해 초기화 되지 않은 인스턴스를 리턴하는 문제가 생길 수 있다.
+ * : uniqueInstance = God(count++) 실행에서 God 생성자 전에 uniqueInstance 는 notnull 이 되어 다른 스레드에서 초기화되지 않은 인스턴스를 리턴받을 수 있다고 한다.
  *
  */
 
